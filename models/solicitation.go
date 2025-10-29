3f112fcda79853d9986523d398259b4e8793f7be
Wellington <wellington@intellisoft.net.br>
2025-10-29 20:09:47 -0300

    solicitation model

--- models/solicitation.go
+++ models/solicitation.go
@@ -6,21 +6,25 @@ import (
 )
 
 type Solicitation struct {
-	ID           uuid.UUID        `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	CustomerID   uuid.UUID        `json:"customer_id"`
-	Customer     Customer         `gorm:"foreignKey:CustomerID;references:ID" json:"customer"`
-	AddressID    uuid.UUID        `json:"address_id"`
-	Address      Address          `gorm:"foreignKey:AddressID;references:ID" json:"address"`
-	Agency       string           `json:"agency"`
-	Services     string           `json:"services"`
-	Status       int              `json:"status"`
-	GasCarrier   string           `json:"gas_carrier"`
-	WaterCarrier string           `json:"water_carrier"`
-	PowerCarrier string           `json:"power_carrier"`
-	Water        bool             `json:"water" gorm:"default:false"`
-	Gas          bool             `json:"gas" gorm:"default:false"`
-	Power        bool             `json:"power" gorm:"default:false"`
-	PIX          ASAASPixResponse `json:"pix" gorm:"-"`
+	ID            uuid.UUID        `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	CustomerID    uuid.UUID        `json:"customer_id"`
+	Customer      Customer         `gorm:"foreignKey:CustomerID;references:ID" json:"customer"`
+	AddressID     uuid.UUID        `json:"address_id"`
+	Address       Address          `gorm:"foreignKey:AddressID;references:ID" json:"address"`
+	Agency        string           `json:"agency"`
+	Services      string           `json:"services"`
+	Status        int              `json:"status"`
+	GasCarrier    string           `json:"gas_carrier"`
+	WaterCarrier  string           `json:"water_carrier"`
+	PowerCarrier  string           `json:"power_carrier"`
+	Water         bool             `json:"water" gorm:"default:false"`
+	Gas           bool             `json:"gas" gorm:"default:false"`
+	Power         bool             `json:"power" gorm:"default:false"`
+	PIX           ASAASPixResponse `json:"pix" gorm:"-"`
+	AgencyId      uuid.UUID        `json:"agency_id"`
+	Service       string           `json:"service"`
+	PaymentType   string           `json:"payment_type"`
+	PaymentStatus string           `json:"payment_status"`
 }
 
 func (s *Solicitation) BeforeCreate(tx *gorm.DB) (err error) {
